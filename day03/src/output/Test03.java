454df7a4159206a3563b8aaed460877f1f5e6020
user1 <user1@DESKTOP-0IFG55Q>
2019-10-21 17:55:34 +0900

    day03

---
+++ day03/src/output/Test03.java
@@ -0,0 +1,15 @@
+package output;
+
+import java.lang.*;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+//		주의사항 : \ 때문에 원치 않는 상황이 발생
+//		
+		System.out.println("나는 지금 \"피자\"가 먹고 싶다");
+		System.out.println("\\\\192.168.20.42");
+		System.out.println("C:\\Windows\\System32");
+	}
+
+}
