76506c0096b222c057c009fc8b5e1967b316d1b9
user1 <user1@DESKTOP-0IFG55Q>
2019-10-25 18:34:20 +0900

    day07

--- day07/src/loop/Test18.java
+++ day07/src/loop/Test18.java
@@ -15,8 +15,16 @@ public class Test18 {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int n = sc.nextInt();
+		boolean b = true;
 		
-
+		for(int i = 2; i < n; i ++) {
+			if(n % i == 0) {
+				System.out.println(n + "은(는) 소수가 아닙니다");
+				b = false;
+				break;
+			}
+		}
+		if(b) System.out.println(n + "은(는) 소수입니다");
 	}
-
 }
