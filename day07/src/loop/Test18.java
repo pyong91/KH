e7f783c6bdfd16239c6932c05e3ce03769b6677c
user1 <user1@DESKTOP-0IFG55Q>
2019-10-28 10:53:30 +0900

    day08

--- day07/src/loop/Test18.java
+++ day07/src/loop/Test18.java
@@ -18,7 +18,18 @@ public class Test18 {
 		int n = sc.nextInt();
 		boolean b = true;
 		
-		for(int i = 2; i < n; i ++) {
+//		for(int i = 2; i < n; i ++) {
+//			if(n % i == 0) {
+//				System.out.println(n + "은(는) 소수가 아닙니다");
+//				b = false;
+//				break;
+//			}
+//		}
+//		if(b) System.out.println(n + "은(는) 소수입니다");
+	
+		
+//		소수일 경우 제곱근까지만 본다	i * i <= n
+		for(int i = 2; i * i <= n; i ++) {
 			if(n % i == 0) {
 				System.out.println(n + "은(는) 소수가 아닙니다");
 				b = false;
@@ -26,5 +37,6 @@ public class Test18 {
 			}
 		}
 		if(b) System.out.println(n + "은(는) 소수입니다");
+		
 	}
 }
---
+++ day08/src/loop/Test19.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class Test19 {
+
+	public static void main(String[] args) {
+//		반복문을 여러번 겹쳐야 하는 상황
+		
+		
+	}
+
+}
