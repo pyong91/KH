768286fc2ab4195963defc660c97e2fb050fedfd
pyong <rushhour9@naver.com>
2019-11-02 15:33:11 +0900

    191102 과제

--- day12/src/oop/modifier6/User.java
+++ day12/src/oop/modifier6/User.java
@@ -18,7 +18,7 @@ public class User {
 		this.calc();
 	}
 	
-	public void calc() {
+	private void calc() {
 		this.setTotal(this.kor + this.eng + this.math);
 		this.setAvg(this.total / 3);
 		this.setPass(this.avg >= 60 ? true : false);
