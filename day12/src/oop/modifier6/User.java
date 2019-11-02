3d5a65fdbae703013635d96b69050b4f23eb29c9
pyong <rushhour9@naver.com>
2019-11-02 14:35:40 +0900

    191102주말과제

--- day12/src/oop/modifier6/User.java
+++ day12/src/oop/modifier6/User.java
@@ -1,13 +1,92 @@
 package oop.modifier6;
 
 public class User {
-//	이름 		국어점수	 영어점수 	수학점수	 총점	 평균	합격/불합격
-//	david	80		70		85		-	-		-
-//	ronaldo	91		53		77		-	-		-
-//	messi	66		89		75		-	-		-
-	
-//	 - 점수는 0 ~ 100점 사이만 설정 가능
-//	 - 총점과 평균은 자동으로 계산
-//	 - 평균이 60점 미만이면 불합격, 60점 이상이면 합격으로 출력 시 표시
+
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int total;
+	private double avg;
+	private boolean isPass;
+
+	User(String name, int kor, int eng, int math) {
+		this.setName(name);
+		this.setKor(kor);
+		this.setEng(eng);
+		this.setMath(math);
+		this.setTotal(kor + eng + math);
+		this.setAvg(this.getTotal() / 3);
+		this.setPass(this.getAvg() >= 60 ? true : false);
+	}
+
+	public void print() {
+		System.out.println("이름  : " + this.getName());
+		System.out.println("국어  : " + this.getKor());
+		System.out.println("영어  : " + this.getEng());
+		System.out.println("수학  : " + this.getMath());
+		System.out.println("총점  : " + this.getTotal());
+		System.out.println("평균  : " + this.getAvg());
+		if (isPass)
+			System.out.println("결과 : 합격");
+		else
+			System.out.println("결과 : 불합격");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public boolean isPass() {
+		return isPass;
+	}
+
+	public void setPass(boolean isPass) {
+		this.isPass = isPass;
+	}
 
 }
